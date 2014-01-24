0c29627aebac106644009dfe68cc726a1b8da63a
Patrick Bogen <pbogen@twitter.com>
2014-01-24 15:47:15 -0800

    More status info for chequebot

--- chequebot.rb
+++ chequebot.rb
@@ -7,8 +7,12 @@ require 'logger'
 DataMapper::Logger.new($stdout, :debug)
 logger = Logger.new( STDOUT )
 
+logger.info "Starting up..."
+
 if ! ENV[ "DATABASE_URL" ]
 then
 	logger.fatal "Cannot start: No DATABASE_URL set in environment"
-	exit
+	exit 1
 end
+
+logger.info "Database URL is there..."
